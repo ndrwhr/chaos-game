787014677b848a4cbc512c1b65395383a0012794
Andrew Wang-Hoyer <me@andrew-hoyer.com>
2017-06-10 22:54:01 -0400

    Close the ColorPicker when the user clicks outside of it

--- src/controls/ColorPicker.js
+++ src/controls/ColorPicker.js
@@ -1,83 +1,103 @@
-import _ from 'lodash';
 import classNames from 'classnames';
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 
 import { COLORS, LIGHT_COLOR_LOOKUP } from '../Options';
 
 import './color-picker.css';
 
-export default (props) => {
-  const hueIndex = 4;
+export default class ColorPicker extends Component {
+  constructor(props){
+    super(props);
+    this.onBodyClick = this.onBodyClick.bind(this);
+  }
 
-  const color = props.color || COLORS[0][hueIndex];
+  componentDidMount(){
+    document.body.addEventListener('click', this.onBodyClick);
+  }
 
-  const shades = COLORS.find(colorList =>
-    colorList.includes(color));
-  const shadeIndex = shades.indexOf(color);
+  componentWillUnmount(){
+    document.body.removeEventListener('click', this.onBodyClick);
+  }
 
-  const hues = COLORS.map(colorList => colorList[hueIndex]);
-  const selectedHue = shades[hueIndex];
+  render(){
+    const props = this.props;
+    const hueIndex = 4;
+    const color = props.color || COLORS[0][hueIndex];
 
-  const selectRandomColor = () => {
-    const shiftedShadeIndex = shadeIndex + _.sample(_.range(-2, 2));
-    const newShadeIndex = _.clamp(shiftedShadeIndex, 0, shades.length);
-    const newColor = _.sample(COLORS)[newShadeIndex];
-    props.onChange(newColor);
-  };
+    const shades = COLORS.find(colorList => colorList.includes(color));
+    const shadeIndex = shades.indexOf(color);
 
-  const classes = classNames('color-picker', {
-    'color-picker--open': props.open,
-  });
+    const hues = COLORS.map(colorList => colorList[hueIndex]);
+    const selectedHue = shades[hueIndex];
 
-  return (
-    <div className={classes}>
-      <div className="color-picker__lists">
-        <div className="color-picker__list color-picker__list--hues">
-          {hues.map(hue => (
-            <button
-              key={`hue-${hue}`}
-              className={
-                classNames('color-picker__button color-picker__button--hue', {
-                  'color-picker__button--dark-text': LIGHT_COLOR_LOOKUP.has(hue),
-                  'color-picker__button--selected': hue === selectedHue,
-                })
-              }
-              style={{background: hue}}
-              onClick={() => props.onChange(hue)}
-            />
-          ))}
+    const selectRandomColor = () => {
+      const shiftedShadeIndex = shadeIndex + _.sample(_.range(-2, 2));
+      const newShadeIndex = _.clamp(shiftedShadeIndex, 0, shades.length);
+      const newColor = _.sample(COLORS)[newShadeIndex];
+      props.onChange(newColor);
+    };
+
+    const classes = classNames('color-picker', {
+      'color-picker--open': props.open,
+    });
+
+    return (
+      <div className={classes}>
+        <div className="color-picker__lists">
+          <div className="color-picker__list color-picker__list--hues">
+            {hues.map(hue => (
+              <button
+                key={`hue-${hue}`}
+                className={
+                  classNames('color-picker__button color-picker__button--hue', {
+                    'color-picker__button--dark-text': LIGHT_COLOR_LOOKUP.has(hue),
+                    'color-picker__button--selected': hue === selectedHue,
+                  })
+                }
+                style={{background: hue}}
+                onClick={() => this.props.onChange(hue)}
+              />
+            ))}
+          </div>
+
+          <div className="color-picker__list color-picker__list--shades">
+            {shades.map((shade, index) => (
+              <button
+                key={`shade-${index}`}
+                className={
+                  classNames('color-picker__button color-picker__button--shade', {
+                    'color-picker__button--dark-text': LIGHT_COLOR_LOOKUP.has(shade),
+                    'color-picker__button--selected': shade === color,
+                  })
+                }
+                style={{background: shade}}
+                onClick={() => this.props.onChange(shade)}
+              />
+            ))}
+          </div>
         </div>
-
-        <div className="color-picker__list color-picker__list--shades">
-          {shades.map((shade, index) => (
-            <button
-              key={`shade-${index}`}
-              className={
-                classNames('color-picker__button color-picker__button--shade', {
-                  'color-picker__button--dark-text': LIGHT_COLOR_LOOKUP.has(shade),
-                  'color-picker__button--selected': shade === color,
-                })
-              }
-              style={{background: shade}}
-              onClick={() => props.onChange(shade)}
-            />
-          ))}
+        <div className="color-picker__controls">
+          <button
+            className="btn btn--inline"
+            onClick={selectRandomColor}
+          >
+            select random
+          </button>
+          <button
+            className="btn btn--inline"
+            onClick={props.onClose}
+          >
+            close
+          </button>
         </div>
       </div>
-      <div className="color-picker__controls">
-        <button
-          className="btn btn--inline"
-          onClick={selectRandomColor}
-        >
-          select random
-        </button>
-        <button
-          className="btn btn--inline"
-          onClick={props.onClose}
-        >
-          close
-        </button>
-      </div>
-    </div>
-  )
-};
+    );
+  }
+
+  onBodyClick(evt){
+    if (!evt.target.matches('.color-picker, .color-picker *') && this.props.open){
+      this.props.onClose();
+    }
+  }
+}
