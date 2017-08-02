6c1b471d8ad1bba550dbf445d7ca0802a169e5e9
Andrew Wang-Hoyer <me@andrew-hoyer.com>
2017-08-01 22:14:20 -0400

    Make preset buttons wrap around instead of being disabled

--- src/components/controls/PresetControl.js
+++ src/components/controls/PresetControl.js
@@ -6,44 +6,53 @@ import Button from './Button';
 
 import './preset-control.css';
 
-export default ({ selectedValue, onChange }) =>
-  <div
-    className={classNames('preset-control', {
-      'preset-control--active': selectedValue,
-    })}
-  >
-    <Button
-      modifiers={{ arrowLeft: true }}
-      disabled={selectedValue === 0 || selectedValue === 1}
-      onPress={() => onChange(selectedValue - 1)}
-    />
+export default ({ selectedValue, onChange }) => {
+  const selectableOptions = CONTROLS[CONTROL_TYPES.PRESET].options.slice(1);
 
-    <div className="preset-control__select-wrapper">
-      <select
-        className="preset-control__select"
-        onChange={({ target }) => onChange(parseInt(target.value, 10))}
-        value={selectedValue}
-      >
-        {!selectedValue &&
-          <option key={0} value={0}>
-            Custom
-          </option>}
-        {CONTROLS[CONTROL_TYPES.PRESET].options
-          .slice(1)
-          .map(({ value, name }, index) =>
+  const getPreviousSelectedValue = () => {
+    const nextValue =
+      selectedValue === 0 || selectedValue === 1
+        ? selectableOptions.length
+        : selectedValue - 1;
+    onChange(nextValue);
+  };
+  const getNextSelectedValue = () => {
+    const nextValue =
+      selectedValue < selectableOptions.length ? selectedValue + 1 : 1;
+    onChange(nextValue);
+  };
+
+  return (
+    <div
+      className={classNames('preset-control', {
+        'preset-control--active': selectedValue,
+      })}
+    >
+      <Button
+        modifiers={{ arrowLeft: true }}
+        onPress={getPreviousSelectedValue}
+      />
+
+      <div className="preset-control__select-wrapper">
+        <select
+          className="preset-control__select"
+          onChange={({ target }) => onChange(parseInt(target.value, 10))}
+          value={selectedValue}
+        >
+          {!selectedValue &&
+            <option key={0} value={0}>
+              Custom
+            </option>}
+          {selectableOptions.map(({ value, name }, index) =>
             <option key={value} value={index + 1}>
               {name}
             </option>,
           )}
-      </select>
-      <div className="preset-control__select-arrow" />
-    </div>
+        </select>
+        <div className="preset-control__select-arrow" />
+      </div>
 
-    <Button
-      modifiers={{ arrowRight: true }}
-      disabled={
-        selectedValue === CONTROLS[CONTROL_TYPES.PRESET].options.length - 1
-      }
-      onPress={() => onChange(selectedValue + 1)}
-    />
-  </div>;
+      <Button modifiers={{ arrowRight: true }} onPress={getNextSelectedValue} />
+    </div>
+  );
+};
